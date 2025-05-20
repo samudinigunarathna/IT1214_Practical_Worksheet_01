d2c1cc90151f551d3a1062634502b0fde1b11ca1
Samudini Gunarathna <77316805+samudinigunarathna@users.noreply.github.com>
2025-05-20 11:37:00 +0000

    Created Setters and getters

--- Q5/BankAccount.java
+++ Q5/BankAccount.java
@@ -1,17 +1,43 @@
 class BankAccount{
+    String name;
     double deposit;
     double withdraw;
     double balance;
 
 }
 
-DisplayBalance(){
-    System.out.println("Your balance is "+)
+SetDeposit(double deposit){
+    this.deposit=deposit;
+}
+
+SetWithdraw(double withdraw){
+    this.withdraw=withdraw;
+}
+
+SetBalance(double balance){
+    this.balance=balance;
+}
+
+int Deposit(){
+    
+    return deposit;
+}
+
+int Withdraw(){
+    
+    return withdraw;
+}
+
+void DisplayBalance(){
+    System.out.println("Your balance is "+ balance);
 }
 
 class App{
     public static void main(String[] args){
         BankAccount ba=new BankAccount();
+        
+        System.out.println("Your Deposit is Rs. "+ba.Deposit());
+        System.out.println("You have withdrawn Rs. "+ba.Withdraw);
         ba.DisplayBalance();
     }
 }
