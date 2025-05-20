93f8ef1ff8984f9f37689aa11f67c1cfb9e331bc
Samudini Gunarathna <77316805+samudinigunarathna@users.noreply.github.com>
2025-05-20 12:18:58 +0000

    Updated values

--- Q6/Book.java
+++ Q6/Book.java
@@ -1,19 +1,19 @@
 class Book{
     private String name;
-    private String Author;
+    private String author;
     private double price;
 
     void setBook(String name, String author, double price){
         this.name=name;
         this.author=author;
         this.price=price;
-        System.out.println("Harry Potter","J.K.Rowling",1600);
+        System.out.println(name+" "+author+" "+price);
     }
 }
 
 class App{
     public static void main(String[] args){
-        Book book=new Book();
-
+        Book bo=new Book();
+        bo.setBook("Harry Potter","J.K.Rowling",1600);
     }
 }
