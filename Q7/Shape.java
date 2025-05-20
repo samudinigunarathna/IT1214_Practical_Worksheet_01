8f9aaff5aa213c695908ccbfc3c041d8f60f55e1
Samudini Gunarathna <77316805+samudinigunarathna@users.noreply.github.com>
2025-05-20 12:42:12 +0000

    Updated

--- Q7/Shape.java
+++ Q7/Shape.java
@@ -1,4 +1,7 @@
 class Shape{
+    double area;
+    private double radius;
+
     void area(){
         System.out.println("Area not Defined");
     }
@@ -7,15 +10,23 @@ class Shape{
 class Circle extends Shape{
     double radius;
 
-    void circle(double radius){
+    void circle(double radius, double area){
+        this.radius=radius;
         this.area=area;
+        System.out.println(radius);
+    }
+
+    void area(){
+        area= radius*3.14;
+        System.out.println("The Area of The Circle is "+area);
     }
 }
 
 class App{
     public static void main(String[] args){
-        Circle.cir=new Circle();
-        cir.circle();
+        Circle cir=new Circle();
+        cir.circle(7,0);
+        cir.area();
 
     }
 }
