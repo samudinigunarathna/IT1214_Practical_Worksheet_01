758bd7f24805d27d2993a6d7b818863cfb376975
Samudini Gunarathna <77316805+samudinigunarathna@users.noreply.github.com>
2025-05-20 12:10:17 +0000

    Created method and a new object

--- Q6/Book.java
+++ Q6/Book.java
@@ -1,9 +1,16 @@
 class Book{
-    
+    private String name;
+    private String Author;
+    private double price;
+
+    void setBook(String name, String author, double price){
+        System.out.println("Harry Potter","J.K.Rowling",1600);
+    }
 }
 
 class App{
     public static void main(String[] args){
-
+        Book book=new Book();
+        
     }
 }
