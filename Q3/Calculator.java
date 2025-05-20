4785b273e9f77512ef0e33f0a0921f808bb88323
Samudini Gunarathna <77316805+samudinigunarathna@users.noreply.github.com>
2025-05-20 10:46:08 +0000

    Created getters

--- Q3/Calculator.java
+++ Q3/Calculator.java
@@ -1,9 +1,19 @@
 class Calculator{
-    
+
+    double Add(int a, int b){
+        return (a + b);
+    }
+    double Add(double a, double b){
+        return (a + b);
+    }
+    double Add(int a, int b, int c){
+        return (a + b + c);
+    }
 }
 
 class App{
     public static void main(String[] args){
         Calculator cal=new Calculator();
+        System.out.println(cal.Add());
     }
 }
