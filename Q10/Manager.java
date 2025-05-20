506f283fe6472bb4ce73abaed79f5ac8a86542db
Samudini Gunarathna <77316805+samudinigunarathna@users.noreply.github.com>
2025-05-20 16:11:38 +0000

    Updated setters

--- Q10/Manager.java
+++ Q10/Manager.java
@@ -2,6 +2,14 @@ class Employee{
     private String name;
     private int id;
     private double salary;
+
+    void setEmployee(String name, int id, double salary){
+        this.name=name;
+        this.id=id;
+        this.salary=salary;
+
+        System.out.println(name+" "+id+" "+" "+salary);
+    }
 }
 
 class Manager extends Employee{
@@ -10,6 +18,6 @@ class Manager extends Employee{
 
 class App{
     public static void main(String[] args){
-
+        
     }
 }
