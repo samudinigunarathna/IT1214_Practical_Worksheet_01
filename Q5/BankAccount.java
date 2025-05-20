f114a383e074501c1918b1ca9410aaf755071afc
Samudini Gunarathna <77316805+samudinigunarathna@users.noreply.github.com>
2025-05-20 11:18:49 +0000

    Created DisplayBalance method and a new object

--- Q5/BankAccount.java
+++ Q5/BankAccount.java
@@ -1,9 +1,17 @@
 class BankAccount{
+    double deposit;
+    double withdraw;
+    double balance;
 
+}
+
+DisplayBalance(){
+    System.out.println("Your balance is "+)
 }
 
 class App{
     public static void main(String[] args){
-        
+        BankAccount ba=new BankAccount();
+        ba.DisplayBalance();
     }
 }
