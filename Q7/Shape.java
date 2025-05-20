54ece66cbdb2171948feaea624a07d81d6952ae3
Samudini Gunarathna <77316805+samudinigunarathna@users.noreply.github.com>
2025-05-20 12:28:03 +0000

    created void methods

--- Q7/Shape.java
+++ Q7/Shape.java
@@ -1,13 +1,21 @@
 class Shape{
-
+    void area(){
+        System.out.println("Area not Defined");
+    }
 }
 
 class Circle extends Shape{
-    
+    double radius;
+
+    void circle(double radius){
+        this.area=area;
+    }
 }
 
 class App{
     public static void main(String[] args){
+        Circle.cir=new Circle();
+        cir.circle();
 
     }
 }
